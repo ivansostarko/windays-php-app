6e938d9db810aa40308ec7266c85a6e5d4cc85cd
Ivan Sostarko <IvanSostarko@users.noreply.github.com>
2018-04-24 21:23:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<h1> Docker Swarm Cluster </h1>";
+echo "<h2>IP Address: ";
+echo $_SERVER['SERVER_ADDR'];
+echo "</h2>";
---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Test extends \PHPUnit\Framework\TestCase
+{
+    public function testTrue()
+    {	
+	    $isTrue = true;
+    	$this->assertTrue($isTrue);
+    }
+    
+   
+}
